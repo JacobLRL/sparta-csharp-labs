ddd7a26d3bfd0061701a0eff4c5cfd84ba36f4d4
JacobLRL <JLittle@spartaglobal.com>
2019-02-18 11:31:21 +0000

    added another lab

---
+++ Labs_Test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_112_Collections;
+
+namespace Labs_Test
+{
+    [TestClass]
+    public class LabsTest
+    {
+        [TestMethod]
+        public void Lab112CollectionsTest()
+        {
+            var instance = new Collections();
+            Assert.AreEqual(224, instance.Collections20MinLab(1,2,3));
+            Assert.AreEqual(22400, instance.Collections20MinLab(10,20,30));
+            Assert.AreEqual(464, instance.Collections20MinLab(2,3,4));
+        }
+    }
+}
