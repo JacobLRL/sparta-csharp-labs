5a4b61d2d20ff1f3e474c90ddf83605c24e1d2d2
JacobLRL <JLittle@spartaglobal.com>
2019-02-25 10:02:32 +0000

    added lab 116

---
+++ Lab_116_Exceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_116_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception("Phils exception");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("something");
+                    throw;
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                Console.WriteLine("done");
+            }
+        }
+    }
+}
